ca46ac95cbcccfeb2eae26c0beeddcc59cd9a396
Luiz Augusto Correa Melo <70124373+Botmano@users.noreply.github.com>
2025-11-16 21:58:16 -0300

    Add files via upload

---
+++ rpg.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+typedef struct personagem
+{
+    char nome[30];
+    int dado;
+    int nivel;
+    int iniciativa;
+}Personagem;
+
+int rolar_dado(int dado){
+  return (1+(rand()%dado)); // Mais 1 para ficar enves de 0 ate o valor do dado menos 1 fica de 1 até o valor do dado;
+}
+
+void calcular_iniciativa(Personagem* p){
+  p->iniciativa = rolar_dado(p->dado)+p->nivel;
+}
+
+Personagem criar_personagem(char nome[30], int dado, int nivel){
+  Personagem p;
+  strcpy(p.nome, nome);
+  p.dado = dado;
+  p.nivel = nivel;
+  p.iniciativa = 0;
+  return p;
+}
+
+int main(){
+  srand(time(NULL));
+}
+
+
+
+
+
+
+
+
+
+
+
+
