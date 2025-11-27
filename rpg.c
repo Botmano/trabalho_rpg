67c10b52e9f0777b82192d7679f2c9be4d0df171
Luiz Augusto Correa Melo <luiz_gforce@outlook.com>
2025-11-26 23:20:35 -0300

    Testando

--- rpg.c
+++ rpg.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define MAX_PERSONAGENS_COMBATE 10;
+#include <stdbool.h>
+#define MAX_PERSONAGENS_COMBATE 10
 
 typedef struct personagem
 {
@@ -10,6 +11,7 @@ typedef struct personagem
     int dado;
     int nivel;
     int iniciativa;
+    bool em_combate;
 }Personagem;
 
 Personagem criar_personagem(char nome[30], int dado, int nivel){
@@ -40,7 +42,7 @@ int particionar(Personagem lista[], int max, int min){
   }
   
   trocar(&lista[i+1], &lista[max]);
-  
+
   return(i+1); 
 }
 
@@ -60,23 +62,40 @@ void calcular_iniciativa(Personagem* p){
   p->iniciativa = rolar_dado(p->dado)+p->nivel;
 }
 
-Personagem criar_personagem(char nome[30], int dado, int nivel){
-  Personagem p;
-  strcpy(p.nome, nome);
-  p.dado = dado;
-  p.nivel = nivel;
-  p.iniciativa = 0;
-  return p;
+void iniciativas_dos_personagens(Personagem participantes[], int num_personagens){
+    for (int i = 0; i < num_personagens; i++){
+        calcular_iniciativa(&participantes[i]);
+    }
 }
-void iniciar_combat(Personagem lista_em_combate[]){
-  
+
+void definir_turnos(Personagem participantes[], int num_personagens){
+  quicksort(participantes, 0, num_personagens);
+}
+
+void iniciar_combate(Personagem lista_em_combate[], int num_participantes){
+    iniciativas_dos_personagens(lista_em_combate, num_participantes);
+    definir_turnos(lista_em_combate, num_participantes);
+
+    for(int i; i<num_participantes; i++){
+      printf("Posição %d, personagem:%s \n", i+1, lista_em_combate[i].nome);
+    }
 }
 
 int main(){
-  Personagem lista_em_combate[MAX_PERSONAGENS_COMBATE];
+  srand(time(NULL)); //Inicialização do timer para ser usado no rand
 
+  Personagem personagens[MAX_PERSONAGENS_COMBATE];
+  int qtd_atual = 0;
+    // 2. Adicionar personagens
+    personagens[qtd_atual++] = criar_personagem("Guerreiro", 20, 5); // d20 + 5
+    personagens[qtd_atual++] = criar_personagem("Mago", 6, 2);       // d6 + 2
+    personagens[qtd_atual++] = criar_personagem("Goblin", 20, 1);    // d20 + 1
+    personagens[qtd_atual++] = criar_personagem("Dragão", 20, 10);   // d20 + 10
 
-  srand(time(NULL));
+    // 3. Iniciar a lógica de combate
+    iniciar_combate(personagens, qtd_atual);
+
+    return 0;
 }
 
 
